a2a58f69b33b668967b53b08fcdc812a974d8df9
iamdeedz <121265222+iamdeedz@users.noreply.github.com>
2024-02-26 21:26:43 +0000

    new project
    alan turing

---
+++ alan_turing/index.js
@@ -0,0 +1,31 @@
+console.log("View Width: " + window.innerWidth);
+console.log("View Height: " + window.innerHeight);
+
+const pages = ["", "loc-fact", "provinces", "national-anthem", "food", "sports", "history", "WWII", "celebs", "fun-facts", "thanks"];
+const pathname_array = document.location.pathname.split("/");
+const current_page = pages.indexOf(pathname_array[pathname_array.length - 1].split(".")[0]);
+
+document.addEventListener("DOMContentLoaded", load);
+
+function load() {
+    document.querySelector("body").classList.add("fade-in")
+    setTimeout('document.querySelector("body").classList.remove("fade-in")', 1000);
+}
+
+function next_page() {
+    document.querySelector("body").classList.add("fade-out");
+    setTimeout('document.location = pages[current_page + 1] + ".html"', 1000);
+}
+
+function prev_page() {
+    document.querySelector("body").classList.add("fade-out");
+
+    // If the previous page is empty (is the index file), go to the root directory
+    if (pages[current_page - 1] == "") {
+        setTimeout('document.location = "."', 1000);
+    }
+
+    else {
+        setTimeout('document.location = pages[current_page - 1] + ".html"', 1000);
+    }
+}
